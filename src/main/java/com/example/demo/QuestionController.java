a157fc67f7365d61b3eb16a6798373ae605660a6
Jiwon Bae <jwb8705@gmail.com>
2017-12-27 16:25:55 +0900

    Adds functions related to qna

---
+++ src/main/java/com/example/demo/QuestionController.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import question.Question;
+
+@Controller
+public class QuestionController {
+	List<Question> questions = new ArrayList<>();
+	@GetMapping("/qna")
+	public String show() {
+		return "qna/form";
+	}
+	@GetMapping("/")
+	public String showQuestions(Model model) {
+		model.addAttribute("questions", questions);
+		return "index";
+	}
+	@PostMapping("/questions")
+	public String ask(Question question) {
+		questions.add(question);
+		return "redirect:/";
+	}
+	@GetMapping("/qna/{id}")
+	public String showDetails(@PathVariable int id, Model model) {
+		Question question = questions.get(id);
+		model.addAttribute("question", question);
+		return "qna/show";
+	}
+}
---
+++ src/main/java/question/Question.java
@@ -0,0 +1,34 @@
+package question;
+
+public class Question {
+	private String writer;
+	private String title;
+	private String contents;
+	private int id;
+
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+}
